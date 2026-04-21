010f20ebbdcc699a446347c6f478d17ab02e71d3
JOSE ANGEL LUGO ESPARZA <jose.lugo@mesa.ms>
2026-04-21 08:19:26 -0600

    feat: nuevo modelo

---
+++ Core/Entities/MasterImprovement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class MasterImprovement
+    {
+        public int Id { get; set; }
+
+        public string ParentPartNumber { get; set; } = null!;
+
+        public int Line { get; set; }
+
+        public decimal OldCycleTime { get; set; }
+
+        public decimal NewCycleTime { get; set; }
+
+        public DateTime ImprovementDate { get; set; }
+
+        public string? Process { get; set; }
+
+        public string? Description { get; set; }
+
+        public decimal TimeSaved => OldCycleTime - NewCycleTime;
+    }
+}
